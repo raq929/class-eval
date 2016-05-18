582c5883beb4d8f2cc934f09c4e2c26876b6adc4
Antony Donovan <antony.donovan@generalassemb.ly>
2016-05-18 11:48:03 -0400

    Silence missing .env message for NODE_ENV=production
    For heroku deployment or similar.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
-
-require('dotenv').config();
+require('dotenv').load({
+  silent: process.env.NODE_ENV === 'production', // don't log missing .env
+});
 
 const express = require('express');
 const app = express();
