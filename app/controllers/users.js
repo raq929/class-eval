0c0e5fec099ae4c1a68f288a9425896b7f1aef25
racheltstevens <racheltstevens@gmail.com>
2016-11-29 10:59:15 -0500

    Replace express-template with express-api-template
    Close #24

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const debug = require('debug')('express-template:users');
+const debug = require('debug')('express-api-template:users');
 
 const controller = require('lib/wiring/controller');
 const models = require('app/models');
