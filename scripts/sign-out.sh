c1395f816576817926130327ba6b546633c15cdd
racheltstevens <racheltstevens@gmail.com>
2016-12-01 16:02:21 -0500

    Update curl requests to be inline with rails-api-template
    - use variables in curl requests
    - close #25

--- scripts/sign-out.sh
+++ scripts/sign-out.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-curl --include --request DELETE http://localhost:4741/sign-out/$ID \
+API="http://localhost:4741"
+URL_PATH="/sign-out"
+
+curl "${API}${URL_PATH}/${ID}" \
+  --include \
+  --request DELETE \
+  --header "Content-Type: application/json" \
   --header "Authorization: Token token=$TOKEN"
+
+echo
