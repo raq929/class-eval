3a3f814fde1d570cbcc6cc6e62b08257f3b06558
racheltstevens <racheltstevens@gmail.com>
2016-12-09 09:57:41 -0500

    Add timestamps and full-name to student data.

--- app/models/student.js
+++ app/models/student.js
@@ -3,14 +3,23 @@
 const mongoose = require('mongoose');
 
 const studentSchema = new mongoose.Schema({
-  first_name: {
-    type: String,
-    required: true,
+    first_name: {
+      type: String,
+      required: true,
+    },
+    last_name: {
+      type: String,
+      required: true,
+    },
   },
-  last_name: {
-    type: String,
-    required: true,
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
   }
+);
+
+studentSchema.virtual('full_name').get(function length() {
+  return `${this.first_name} ${this.last_name}`;
 });
 
 const Student = mongoose.model('Student', studentSchema);
