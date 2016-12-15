dc8c1ff3b995b27eaab23168a704a3ddfc04ac4e
racheltstevens <racheltstevens@gmail.com>
2016-12-15 18:05:11 -0500

    Refactor adding instructors
    Create separate controller action

--- app/controllers/rosters.js
+++ app/controllers/rosters.js
@@ -64,14 +64,6 @@ const update = (req, res, next) => {
         roster.students.addToSet.apply(roster.students, newStudents);
         delete req.body.students; // don't include students in the general update
       }
-
-      let newInstructors = req.body.roster.instructors ? req.body.roster.instructors : null;
-      let currentInstructors = roster._instructors;
-      if(newInstructors) {
-        currentInstructors.addToSet.apply(currentInstructors, newInstructors);
-        delete req.body._instructors; // don't include instructors in the general update
-      }
-
       if(req.body.name) {
         roster.update({ name: req.body.name })
         .catch(err => next(err));
@@ -100,6 +92,22 @@ const removeStudent = (req, res, next) => {
     .catch(err => next(err));
 };
 
+const addInstructor = (req, res, next) => {
+  let search = { _id: req.params.id };
+  Roster.findOne(search)
+    .then(roster => {
+      let newInstructors = req.body.roster.instructors ? req.body.roster.instructors : null;
+      let currentInstructors = roster._instructors;
+      if(newInstructors) {
+        currentInstructors.addToSet.apply(currentInstructors, newInstructors);
+        delete req.body._instructors; // don't include instructors in the general update
+        return roster.save();
+      }
+    })
+    .then(( /* roster */ ) => res.sendStatus(200))
+    .catch(err => next(err));
+};
+
 const removeInstructor = (req, res, next) => {
   let search = { _id: req.params.id };
   Roster.findOne(search)
@@ -110,11 +118,11 @@ const removeInstructor = (req, res, next) => {
       const instructors = roster._instructors;
       if(instructors.length >= 0 && req.body.roster && req.body.roster.instructor) {
         return roster.update({
-          $pull: { '_instructors': req.body.roster.instructor}
-        });
+          $pull: { '_instructors': req.body.roster.instructor }
+        })
+        .then(() => res.sendStatus(200));
       }
     })
-    .then(() => res.sendStatus(200))
     .catch(err => next(err));
 };
 
@@ -126,7 +134,8 @@ module.exports = controller({
   show,
   update,
   removeStudent,
+  addInstructor,
   removeInstructor,
 }, { before: [
-  { method: authenticate, except: ['index', 'show'] },
+  { method: authenticate },
 ], });
